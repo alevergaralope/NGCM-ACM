03b4db224f15bc1cab8a9ad357676d77b89c77b9
Norma Alejandra <nvlg1e15@Normas-Air.lan>
2016-02-22 16:47:33 +0000

    add test fibonacci correct

--- test_fibonacci.py
+++ test_fibonacci.py
@@ -1,10 +1,10 @@
 def test_fibonacci():
-   """
-   In this function we import file fibonacci.py
-   and test the function inside it, Fibonacci(n)
-   against known values
-   """
+    """
+    In this function we import file fibonacci.py
+    and test the function inside it, Fibonacci(n)
+    against known values
+    """
 
- import fibonacci as fb
- assert fb.Fibonacci(10) == 55
- assert fb.Fibonacci(5) == 5
+    import fibonacci as fb
+    assert fb.Fibonacci(10) == 55
+    assert fb.Fibonacci(5) == 5
